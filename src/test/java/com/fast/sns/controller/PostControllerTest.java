e84739ba131ff41d389441ed21c9d076b2a40324
JUNGSANGYUN <sdsd98987@gmail.com>
2024-04-18 17:46:34 +0900

    fix: 댓글 관련 테스트 코드 변경
    댓글 기능 코드로 인한 테스트 코드 변경

--- src/test/java/com/fast/sns/controller/PostControllerTest.java
+++ src/test/java/com/fast/sns/controller/PostControllerTest.java
@@ -296,7 +296,7 @@ public class PostControllerTest {
     @WithAnonymousUser
     void 댓글작성시_게시물이_없는경우() throws Exception {
 
-        doThrow(new SnsApplicationException(ErrorCode.POST_NOT_FOUND)).when(postService).comment(any(),any());
+        doThrow(new SnsApplicationException(ErrorCode.POST_NOT_FOUND)).when(postService).comment(any(),any(),any());
 
         mockMvc.perform(post("/api/v1/posts/1/comments")
                         .contentType(MediaType.APPLICATION_JSON)
